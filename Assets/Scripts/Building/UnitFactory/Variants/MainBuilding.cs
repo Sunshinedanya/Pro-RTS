7ffcaf02b7b532b74b833ac52279c4468a714ed6
Даниил <144004357+Sunshinedanya@users.noreply.github.com>
2024-06-17 14:54:50 +0300

    очистка в некотором радиусе готова

---
+++ Assets/Scripts/Building/UnitFactory/Variants/MainBuilding.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MainBuilding : BuildingCompopnent<Building>
+{
+    [SerializeField] private float radius;
+    private void Start()
+    {
+        print("start");
+        ClearSpace(radius);
+    }
+
+    private void ClearSpace(float radius)
+    {
+        var colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (var collider in colliders)
+        {
+            print(collider);
+            if (collider.gameObject.layer == 7)
+            {
+                Destroy(collider.gameObject);
+            }
+        }
+    }
+}
+
